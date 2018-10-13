f56dd3fd836f0a52af18dbacb2540ddab7dfe9fe
frankhart2017 <sdharchou@gmail.com>
2018-10-13 17:04:12 +0530

    Changes

--- arogya_model_app/views.py
+++ arogya_model_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from arogya_model_app.predict_image import predict
+from arogya_model_app.minify import minify_url
+import logging
+
+def get_current_path(request):
+    return {
+       'current_path': request.get_full_path()
+     }
 
 # Create your views here.
 def index(requests):
-    img = requests.GET['img']
+    img = get_current_path(requests)['current_path']
+    img = img.split("img=")[1]
+    img = minify_url(img)
     prediction = predict(img)
     return JsonResponse({'prediction': prediction})
